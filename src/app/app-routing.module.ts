7ebca19264a03d6596a4e077a2dc5f7960f2e37c
Diego Fernandez <diegoafanador10@gmail.com>
2024-02-18 23:05:36 -0400

    [FEAT]added view components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './components/products/products.component';
+import { PicoPreviewComponent } from './components/pico-preview/pico-preview.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'products',
+    title: 'Products',
+    component: ProductsComponent
+    // loadComponent: () => import('./components/products/products.component').then(m => m.ProductsComponent)
+  },
+  {
+    path: 'pico',
+    title: 'Pico-preview',
+    component: PicoPreviewComponent
+    // loadComponent: () => import('./components/pico-preview/pico-preview.component').then(m => m.PicoPreviewComponent)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
