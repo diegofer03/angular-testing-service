c548abb0b83d7713ccccbbff2ba7398a648fb19f
Diego Fernandez <diegoafanador10@gmail.com>
2024-02-19 20:21:25 -0400

    [FEAT]added test for get request

--- src/app/models/app.mocks.ts
+++ src/app/models/app.mocks.ts
@@ -3,7 +3,7 @@ import { Product } from './app.models';
 
 export const generateOneProduct = (): Product => {
   return {
-    id: faker.datatype.uuid(),
+    id: faker.number.int().toString(),
     title: faker.commerce.productName(),
     price: parseInt(faker.commerce.price(), 10),
     description: faker.commerce.productDescription(),
@@ -11,14 +11,14 @@ export const generateOneProduct = (): Product => {
     id: faker.datatype.number(),
     name: faker.commerce.department()
     },
-    images: [faker.image.imageUrl(), faker.image.imageUrl()]
+    images: [faker.image.url(), faker.image.url()]
   };
 }
 
-export const generateManyProducts = (size = 10): Product[] => {
-  const products: Product[] = [];
-  for (let index = 0; index < size ; index++) {
-    products.push(generateOneProduct());
-  }
-  return [...products];
-}
+export const generateManyProducts = (size = 10): Product[] => new Array(size).fill(null).map(() => ({...generateOneProduct()}))
+  // const products: Product[] = [];
+  // for (let index = 0; index < size ; index++) {
+  //   products.push(generateOneProduct());
+  // }
+  // return [...products];
+
--- src/app/services/products/products.service.spec.ts
+++ src/app/services/products/products.service.spec.ts
@@ -3,12 +3,12 @@ import { HttpClientTestingModule, HttpTestingController } from '@angular/common/
 import { ProductsService } from './products.service';
 import { Product } from './../../models/app.models';
 import { environment } from 'src/environments/environment.development';
-import { generateManyProducts } from 'src/app/models/app.mocks';
+import { generateManyProducts, generateOneProduct } from 'src/app/models/app.mocks';
 
 fdescribe('ProductsService', () => {
   let service: ProductsService;
   let httpController: HttpTestingController
-  const url = `${environment.API_URL}/api/v1/products`
+  const url = `${environment.API_URL}/api/v1`
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -29,16 +29,69 @@ fdescribe('ProductsService', () => {
 
   describe('getAllSimple', () => {
     it('Should get response from products Api', (doneFn) => {
-      const mockData: Product[] = generateManyProducts()
+      const mockData: Product[] = generateManyProducts(2)
       service.getAllSimple().subscribe((data)=>{
         expect(data).toEqual(mockData)
         doneFn()
       })
       //expect one request with an url
-      const req = httpController.expectOne(url)
+      const req = httpController.expectOne(`${url}/products`)
       //flush request with expected mocked data
       req.flush(mockData)
       //controller verify request
+      httpController.verify()
+    })
+  })
+
+  describe('getAll', () => {
+    it('Should get response from all products', (doneFn)=> {
+      const mockData: Product[] = generateManyProducts()
+      service.getAll().subscribe((data)=> {
+        expect(data.length).toEqual(mockData.length)
+        doneFn()
+      })
+      const req = httpController.expectOne(`${url}/products`)
+      req.flush(mockData)
+      httpController.verify()
+    })
+    it('should get all products with taxes', (doneFn) => {
+      const mockData: Product[] = [
+        {
+          ...generateOneProduct(),
+          price: 0
+        },
+        {
+          ...generateOneProduct(),
+          price: -100
+        },
+        {
+          ...generateOneProduct()
+        }
+      ]
+      service.getAll().subscribe((data) => {
+        expect(data[0].taxes).toBe(0)
+        expect(data[2].taxes).toBeDefined()
+        doneFn()
+      })
+      const req = httpController.expectOne(`${url}/products`)
+      req.flush(mockData)
+      httpController.verify()
+    })
+    it('Should get products with offset and limit params', (doneFn) => {
+      const mockData: Product[] = generateManyProducts(3);
+      const limit = 10;
+      const offset = 3;
+
+      service.getAll(limit, offset).subscribe((data) => {
+        expect(data.length).toBe(mockData.length)
+        doneFn()
+      })
+
+      const req = httpController.expectOne(`${url}/products?limit=${limit}&offset=${offset}`)
+      req.flush(mockData)
+      const params = req.request.params
+      expect(params.get('limit')).toBe(limit+'')
+      expect(params.get('offset')).toBe(`${offset}`)
       httpController.verify()
     })
   })
